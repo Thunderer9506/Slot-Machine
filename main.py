b958618a66c2af11fca36d4aad645b98b304b839
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-04-03 22:46:21 +0530

    Made a function get_number_of_lines and it works same as deposti function and all of these function are in main fucntion

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? ₹")
@@ -8,4 +10,22 @@ def deposit():
                 print("Amount must be greater than 0")
         else:
             print("Please enter a number")
-    return amount       
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-"+str(MAX_LINES)+")? ")
+        if lines.isdigit():
+            if 1<= int(lines) <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number")
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+
+main()
